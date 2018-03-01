65d845985a0500307735e7999ba776ab7322c251
Porter <lporter@netsuite.com>
2018-03-01 00:34:46 -0600

    Updates to list controller

--- routes/list.js
+++ routes/list.js
@@ -1,8 +1,20 @@
 /* GET 'list' page */
+module.exports.noteslist = function(req, res){
+    const path = '/api/locations';
+    const requestOptions = {
+        url : apiOptions.server + path,
+        method : 'GET',
+        json : {}
+    };
+    request(
+        requestOptions,
+        (function (err, response, body) {
+            _renderList(req, res, body);
+        })
+    );
+};
 const list = function(req, res){
     res.render('list', { title: 'List of Notes' });
 };
 
-module.exports = {
-    list
-};
+module.exports.list = list;

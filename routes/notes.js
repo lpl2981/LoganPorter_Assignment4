819313214ff90d1b65b3c27e22f8b73ee6db33cf
Porter <lporter@netsuite.com>
2018-03-02 09:26:00 -0600

    Fixes to view to improve the notes page

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,34 @@
-/* GET 'list' page */
-const notes = function(req, res){
-    var notes = [{"name": "groceries"}]
-    //Call API to get array of data
+/* GET 'note' page */
+const request = require('request');
 
-    res.render('notes', {title: 'Note Editor', notes: notes});
+const apiOptions = {
+    server: 'http://localhost:3000'
+};
+if (process.env.NODE_ENV === 'production') {
+    apiOptions.server = 'https://pure-temple-67771.herokuapp.com';
+}
+const _renderNote = function(req, res, responseBody){
+    res.render('notes', { title: 'Note Editor', notes: responseBody });
 };
 
-module.exports.note = notes;
+const noteslist = function(req, res){
+    const path = '/api/notes';
+    const requestOptions = {
+        url : apiOptions.server + path,
+        method : 'GET',
+        json : {}
+    };
+    request(
+        requestOptions,
+        (function (err, response, body) {
+            console.log(err, body);
+            _renderNote(req, res, body);
+        })
+    );
+};
+const notes = function(req, res) {
+    var notes = [{"name": "groceries"}];
+    //Call API to get array of data
+};
+
+module.exports.note = noteslist;
